3cdcd2dbf881d431ecd14689a91f0cda5a8e85e8
Guanzhou Hu <me@josehu.com>
2025-03-15 23:40:36 -0500

    merge leptos+trunk+tailwind template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use leptos::prelude::*;
+
+use leptos_meta::{provide_meta_context, Link, Stylesheet, Title};
+
+use leptos_router::components::{Route, Router, Routes};
+use leptos_router::StaticSegment;
+
+#[component]
+pub fn App() -> impl IntoView {
+    provide_meta_context();
+
+    view! {
+        <Stylesheet id="leptos" href="/style/output.css"/>
+        <Link rel="shortcut icon" type_="image/png" href="/public/favicon.png"/>
+        <Router>
+            <Routes fallback=|| "Page not found.">
+                <Route path=StaticSegment("") view=Home/>
+            </Routes>
+        </Router>
+    }
+}
+
+#[component]
+fn Home() -> impl IntoView {
+    let (value, set_value) = signal(0);
+
+    // thanks to https://tailwindcomponents.com/component/blue-buttons-example for the showcase layout
+    view! {
+        <Title text="Leptos + Tailwindcss"/>
+        <main>
+            <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
+                <div class="flex flex-row-reverse flex-wrap m-auto">
+                    <button on:click=move |_| set_value.update(|value| *value += 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                        "+"
+                    </button>
+                    <button class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white">
+                        {value}
+                    </button>
+                    <button
+                        on:click=move |_| set_value.update(|value| *value -= 1)
+                        class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white"
+                        class:invisible=move || {value.get() < 1}
+                    >
+                        "-"
+                    </button>
+                </div>
+            </div>
+        </main>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    _ = console_log::init_with_level(log::Level::Info);
+    console_error_panic_hook::set_once();
+
+    mount_to_body(App)
 }
